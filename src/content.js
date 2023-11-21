970252b7a48cbacf47979738995cacab97e0565a
이일훈 <rival6821@naver.com>
2023-11-21 17:03:00 +0900

    content작업중

--- src/content.js
+++ src/content.js
@@ -1,3 +1,5 @@
+const template = document.getElementById('li_template');
+
 const create = async () => {
     const result = await fetch("https://url.ilhoon.kr", {
         method: "POST",
@@ -7,5 +9,12 @@ const create = async () => {
         body: JSON.stringify({
             url: window.location.href,
         }),
+    });
+
+    const template = document.getElementById('li_template');
+    const element = template.content.firstElementChild.cloneNode(true);
+    element.querySelector('.url').textContent = result.text();
+    element.querySelector('.paste_button').addEventListener('click', () => {
+        navigator.clipboard.writeText(result.text());
     });
 };
