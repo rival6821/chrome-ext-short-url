e9ef18c19f393a3307e856197a5f6baa15f0f25b
ilhoon <rival6821@naver.com>
2023-11-21 23:04:13 +0900

    url 생성 처리

--- src/content.js
+++ src/content.js
@@ -1,20 +1,25 @@
 const template = document.getElementById('li_template');
 
 const create = async () => {
+    // 현재 열린 탭의 url을 가져온다.
+    const nowUrl = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+    }).then((tabs) => tabs[0].url);
     const result = await fetch("https://url.ilhoon.kr", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            url: window.location.href,
+            url: nowUrl,
         }),
-    });
-
-    const template = document.getElementById('li_template');
-    const element = template.content.firstElementChild.cloneNode(true);
-    element.querySelector('.url').textContent = result.text();
-    element.querySelector('.paste_button').addEventListener('click', () => {
-        navigator.clipboard.writeText(result.text());
-    });
+    }).then((res) => res.json());
+    if (result.error) {
+        alert(result.error);
+        return;
+    }
+    document.getElementById('url').innerText = `https://url.ilhoon.kr/${result.url}`;
 };
+
+document.getElementById('create_btn').addEventListener('click', create);
