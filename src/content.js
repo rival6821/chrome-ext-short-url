91a99f84affbeb84ec41acfb77dae228f23ce890
이일훈 <rival6821@naver.com>
2023-11-22 13:40:40 +0900

    http 포함 및 tailwind 추가

--- src/content.js
+++ src/content.js
@@ -1,11 +1,14 @@
-const template = document.getElementById('li_template');
-
 const create = async () => {
-    // 현재 열린 탭의 url을 가져온다.
     const nowUrl = await chrome.tabs.query({
         active: true,
         currentWindow: true,
     }).then((tabs) => tabs[0].url);
+
+    if (!nowUrl.includes('http://') || !nowUrl.includes('https://')) {
+        alert('http:// 또는 https://로 시작하는 주소만 가능합니다.');
+        return;
+    }
+
     const result = await fetch("https://url.ilhoon.kr", {
         method: "POST",
         headers: {
@@ -15,10 +18,12 @@ const create = async () => {
             url: nowUrl,
         }),
     }).then((res) => res.json());
+
     if (result.error) {
         alert(result.error);
         return;
     }
+
     document.getElementById('url').innerText = `https://url.ilhoon.kr/${result.url}`;
 };
 
