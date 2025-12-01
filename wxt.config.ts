e64c83cc58cce88a93ad0d2f7981feaf6ffc4c96
이일훈 <rival6821@doublemedia.co.kr>
2025-12-01 10:02:42 +0900

    fix: 기본 언어를 한국어에서 영어로 변경

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'wxt';
 
 export default defineConfig({
   manifest: {
-    default_locale: "ko",
+    default_locale: "en",
     name: "__MSG_extName__",
     description: "__MSG_extDescription__",
     version: "1.4",
